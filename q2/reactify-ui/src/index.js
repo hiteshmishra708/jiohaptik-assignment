34fb6bca475c0268cad07c789a2daf302d2a021e
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-04-03 14:44:28 +0530

    Initial changes

---
+++ q2/reactify-ui/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render((
+  <h1>Hello World</h1>
+  ), document.getElementById('reactify-django-ui'));
